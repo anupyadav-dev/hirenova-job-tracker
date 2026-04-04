73cd4a9a7ec26fe7da717bac568d19bb5f21a3a9
Pradumby <anupy08115@gmail.com>
2026-04-04 08:20:03 +0530

    chore(frontend): remove unused component

--- hirenova-frontend/src/App.jsx
+++ hirenova-frontend/src/App.jsx
@@ -19,7 +19,6 @@ import Footer from "./components/layout/Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 function App() {
   return (
     <BrowserRouter>

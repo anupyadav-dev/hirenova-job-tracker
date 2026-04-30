a64e1c1bb5c1b843696dfd2b3ff62335fab3396f
Pradumby <anupy08115@gmail.com>
2026-04-30 14:06:04 +0530

    feat(user): implement user module with admin controls
    - added get current user
    - added admin user listing
    - implemented block/unblock system
    - secured routes using RBAC

--- hirenova-backend/src/modules/user/user.controller.js
+++ hirenova-backend/src/modules/user/user.controller.js
@@ -1,10 +1,30 @@
+import {
+  getMeService,
+  getAllUsersService,
+  updateUserStatusService,
+} from "./user.service.js";
+
 import { asyncHandler } from "../../utils/asyncHandler.util.js";
-import { getProfileService } from "./user.service.js";
 import { ApiResponse } from "../../utils/apiResponse.js";
 
-export const getProfileController = asyncHandler(async (req, res) => {
-  const userId = req.user.id;
-  const user = await getProfileService(userId);
+export const getMeController = asyncHandler(async (req, res) => {
+  const user = await getMeService(req.user._id);
 
-  res.status(200).json(new ApiResponse(200, "User fetched", user));
+  res.status(200).json(new ApiResponse(200, "User fetched successfully", user));
+});
+
+export const getAllUsersController = asyncHandler(async (req, res) => {
+  const users = await getAllUsersService();
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Users fetched successfully", users));
+});
+
+export const updateUserStatusController = asyncHandler(async (req, res) => {
+  const { status } = req.body;
+
+  const user = await updateUserStatusService(req.params.id, status);
+
+  res.status(200).json(new ApiResponse(200, "User status updated", user));
 });
--- hirenova-backend/src/modules/user/user.service.js
+++ hirenova-backend/src/modules/user/user.service.js
@@ -1,10 +1,32 @@
 import User from "./user.model.js";
 import { ApiError } from "../../utils/apiError.js";
 
-export const getProfileService = async (userId) => {
+export const getMeService = async (userId) => {
   const user = await User.findById(userId).select("-password");
+
   if (!user) {
     throw new ApiError(404, "User not found");
   }
+
+  return user;
+};
+
+export const getAllUsersService = async () => {
+  const users = await User.find().select("-password");
+
+  return users;
+};
+
+export const updateUserStatusService = async (userId, status) => {
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  user.status = status;
+
+  await user.save();
+
   return user;
 };
